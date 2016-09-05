294958a185e002cb9dfe7f647bc5499c1ae65bd0
Sergey Panfilov <sergey.panfilov@kpn.com>
2016-09-05 18:28:29 +0200

    FIX threaded backend and tests

--- time_execution/backends/threaded.py
+++ time_execution/backends/threaded.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import datetime
 import logging
 import threading
+import time
 
 from time_execution.backends.base import BaseMetricsBackend
 
@@ -27,6 +28,8 @@ class ThreadedBackend(BaseMetricsBackend):
         self.worker_limit = worker_limit
         self.thread = None
         self.fetched_items = 0
+        self.bulk_size = 50
+        self.bulk_timeout = 1  # second
         self.backend = backend(*backend_args, **backend_kwargs)
         self._queue = Queue(maxsize=queue_maxsize)
         self.start_worker()
@@ -46,15 +49,39 @@ class ThreadedBackend(BaseMetricsBackend):
         self.thread.daemon = True
         self.thread.start()
 
+    def batch_ready(self, batch):
+        return self.bulk_size < len(batch)
+
+    def batch_time(self, last_write):
+        return (time.time() - last_write) >= self.bulk_timeout
+
+    def has_work(self):
+        if self.worker_limit is None:
+            return True
+        return self.fetched_items < self.worker_limit
+
     def worker(self):
-        while (self.worker_limit is None) or self.fetched_items < (self.worker_limit - 1):
+        metrics = []
+        last_write = time.time()
+
+        def send_metrics():
             try:
-                name, data = self._queue.get(True, self.queue_timeout)  # blocking get
+                self.backend.bulk_write(metrics)
+            except Exception as exc:
+                logger.warning('%r write failure %r', self.backend, exc)
+
+        while self.has_work():
+            if self.batch_ready(metrics) or (self.batch_time(last_write) and metrics):
+                send_metrics()
+                last_write = time.time()
+                metrics = []
+            try:  # blocking get
+                name, data = self._queue.get(True, self.queue_timeout)
             except Empty:
                 continue
             self.fetched_items += 1
-            try:
-                self.backend.write(name, **data)
-            except Exception as exc:
-                logger.warning('%r write failure %r', self.backend, exc)
+            data['name'] = name
+            metrics.append(data)
+        if metrics:
+            send_metrics()
         self.thread = None
