071385007367c166ea5c021787ac29ffd3f12d4b
Bart Veraart <bart.veraart@kpn.com>
2017-04-24 11:14:15 +0200

    FIX end the thread if the parent thread died

---
+++ tests/dummy_process.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+import sys
+
+# make sure we can import time_execution library
+path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append('/'.join(path.split("/")[:-1]))
+
+from time_execution.backends.base import BaseMetricsBackend  # noqa isort:skip
+from time_execution.backends.threaded import ThreadedBackend  # noqa isort:skip
+
+
+class DummyBackend(BaseMetricsBackend):
+    def write(self, name, **data):
+        pass
+
+
+ThreadedBackend(DummyBackend, queue_timeout=1)
